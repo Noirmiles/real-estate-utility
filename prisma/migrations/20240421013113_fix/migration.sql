6d94b2e150a9a29242d01a6861c740ea2fe3208c
domoniquesmartt <48745515+domoniquesmartt@users.noreply.github.com>
2024-04-21 16:59:50 -0500

    Updated Migration
    migration history

---
+++ prisma/migrations/20240421013113_fix/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `roleId` on the `Agent` table. All the data in the column will be lost.
+  - You are about to drop the `Permission` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Role` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_RolePermissions` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+
+
+
