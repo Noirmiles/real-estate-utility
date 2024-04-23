f20f269925b3f795873a3ebd4e435b5b89ee3436
domoniquesmartt <48745515+domoniquesmartt@users.noreply.github.com>
2024-04-23 01:01:46 -0500

    Portal In sync
    ---Roles and tabs in sync with user

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -1,15 +1,17 @@
-'use client'
-import React, { useState } from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
-import SearchBar from '@/components/searchBar'; 
+import SearchBar from '@/components/searchBar';
 import Background from '@/components/background';
 import Profile from '@/components/profile/client';
 import Image from 'next/image';
+import { getCurrentUser } from '@/app/services/auth.service';  // Import the getCurrentUser function
+import { IUser } from '../types/user-types';
 
 interface TabPanelProps {
   children?: React.ReactNode;
-  index: any;
-  value: any;
+  index: string;
+  value: string;
 }
 
 function TabPanel(props: TabPanelProps) {
@@ -23,105 +25,96 @@ function TabPanel(props: TabPanelProps) {
       aria-labelledby={`tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <>{children}</>
-      )}
+      {value === index && <>{children}</>}
     </div>
   );
 }
 
 export default function Portal() {
   const [activeTab, setActiveTab] = useState('searches');
-  const [showImage, setShowImage] = useState(true); // State to control image visibility
+  const [showImage, setShowImage] = useState(true);
+  const [user, setUser] = useState<IUser | null>(null);
+
+  useEffect(() => {
+    setUser(getCurrentUser());
+  }, []);
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
     setShowImage(false);  // Hide image whenever a tab is clicked
   };
 
+  const isUser = user && user.role.name === 'user';
+  const isAgent = user && user.role.name === 'agent';
+  const isManager = user && user.role.name == 'manager';
+
   return (
     <div>
       <Background />
       <div className="client-portal">
         <div className="tab-navigation">
-          <Button onClick={() => handleTabClick('profile')}>
-            Profile
-          </Button>
-          <Button onClick={() => handleTabClick('scheduling')}>
-            Scheduling
-          </Button>
-          <Button onClick={() => handleTabClick('searches')}>
-            Saved Searches
-          </Button>
-          <Button onClick={() => handleTabClick('homes')}>
-            Saved Homes
-          </Button>
-          <Button onClick={() => handleTabClick('message')}>
-            Messages
-          </Button>
-          <Button onClick={() => handleTabClick('setting')}>
-            Account Settings
-          </Button>
-          <Button onClick={() => handleTabClick('passcodes')}>
-            Passcodes
-          </Button>
-          <Button onClick={() => handleTabClick('showings')}>
-            Showings
-          </Button>
+          {(isUser || isAgent || isManager) && (
+            <>
+              <Button onClick={() => handleTabClick('profile')}>Profile</Button>
+              <Button onClick={() => handleTabClick('message')}>Messages</Button>
+            </>
+          )}
+        {isAgent &&<Button onClick={() => handleTabClick('scheduling')}>Scheduling</Button>}
+        {isAgent && <Button onClick={() => handleTabClick('passcodes')}>Passcodes</Button>}
+        {isAgent &&<Button onClick={() => handleTabClick('showings')}>Showings</Button>}
+        {isAgent && <Button onClick={() => handleTabClick('listing')}>Listing</Button>}
+        {isManager && <Button onClick={() => handleTabClick('reports')}>Reports</Button>}
         </div>
 
-        <TabPanel value={activeTab} index="profile">
-          <Profile />  {/* Render Profile Component */}
-        </TabPanel>
-
+        {(isUser || isAgent || isManager) && (
+          <>
+            <TabPanel value={activeTab} index="profile">
+              <Profile />
+            </TabPanel>
+            <TabPanel value={activeTab} index="message">
+              <div className="tab-content">
+                <h2>Messages</h2>
+              </div>
+            </TabPanel>
+          </>
+        )}
+        {isAgent && (
         <TabPanel value={activeTab} index="scheduling">
           <div className="tab-content">
             <h2>Schedule</h2>
-            {/* Content for scheduling */}
           </div>
         </TabPanel>
-
-        <TabPanel value={activeTab} index="searches">
-          <div className="tab-content">
-            <h2>Saved Searches</h2>
-            {/* Content for saved searches */}
-          </div>
-        </TabPanel>
-
-        <TabPanel value={activeTab} index="homes">
-          <div className="tab-content">
-            <h2 style={{fontSize: '24px', color: 'white', fontWeight: 'bold'}}>Saved Homes</h2>
-            {/* Content for saved homes */}
-          </div>
-        </TabPanel>
-
-        <TabPanel value={activeTab} index="message">
-          <div className="tab-content">
-            <h2>Messages</h2>
-            {/* Content for messages */}
-          </div>
-        </TabPanel>
-      
-        <TabPanel value={activeTab} index="setting">
-          <div className="tab-content">
-            <h2>Account Settings</h2>
-            {/* Content for account settings */}
-          </div>
-        </TabPanel>
-        
+         )}
+        {isAgent && (
         <TabPanel value={activeTab} index="passcodes">
           <div className="tab-content">
             <h2>Passcodes</h2>
-            {/* Content for account settings */}
           </div>
         </TabPanel>
+        )}
+        {isAgent && (
         <TabPanel value={activeTab} index="showings">
           <div className="tab-content">
             <h2>Showings</h2>
-            {/* Content for account settings */}
           </div>
         </TabPanel>
-      
+        )}
+        {isAgent && (
+        <TabPanel value={activeTab} index="listing">
+          <div className="tab-content">
+            <h2>Listing</h2>
+          </div>
+        </TabPanel>
+        )}
+
+        {isManager && (
+          <TabPanel value={activeTab} index="reports">
+            <div className="tab-content">
+              <h2>Reports</h2>
+            </div>
+          </TabPanel>
+        )}
+
         {showImage && (
           <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0', marginTop: '150px'}}>
             <Image
@@ -141,7 +134,3 @@ export default function Portal() {
     </div>
   );
 }
-
-
-
-
